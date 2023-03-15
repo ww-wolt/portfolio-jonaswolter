3cf623af98b91b5f5f8f3902fb687686ae5e343c
Jonas Wolter <jonas.wolter.ch@gmail.com>
2023-03-15 16:16:08 +0100

    Vercel deyployment options

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			runtime: 'edge',
+			regions: 'all'
+		})
 	},
-  preprocess: vitePreprocess()
+	preprocess: vitePreprocess()
 };
 
 export default config;
