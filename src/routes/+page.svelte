60a4e2de55dc7e918b728778ab6d1c29a053c066
Jonas Wolter <jonas.wolter.ch@gmail.com>
2023-03-15 16:53:33 +0100

    Gsap deployment fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,24 @@
 <script>
 	import gsap from 'gsap/dist/gsap';
 	import ScrollTrigger from 'gsap/dist/ScrollTrigger';
-	gsap.registerPlugin(ScrollTrigger);
+
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		gsap.registerPlugin(ScrollTrigger);
+		gsap.to('.title', {
+			y: '50vh',
+			ease: 'none',
+			scrollTrigger: {
+				trigger: '.title',
+				scrub: true
+			}
+		});
+	});
 </script>
 
-<div class="flex h-[100vh] w-full flex-col items-center justify-center bg-gray-100">
-	<h1 class=" m-6 text-center text-4xl">This site is currently under construction.</h1>
+<div class="flex h-[200vh] w-full flex-col items-center justify-center bg-gray-100">
+	<h1 class=" title m-6 text-center text-4xl">This site is currently under construction.</h1>
 	<h2 class=" text-center text-2xl">Please check again after 3 april.</h2>
 	<div class="absolute bottom-6 text-center">
 		<p class="text-sm">Portfolio</p>
