191402a4793a5e41f745ab2f197c1680fad1b813
Jonas Wolter <jonas.wolter.ch@gmail.com>
2023-03-21 15:07:16 +0100

    deploy test without work redirect

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -2,7 +2,9 @@
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 
-	onMount(async () => {
-		goto('/#work');
-	});
+	// onMount(async () => {
+	// 	goto('/#work');
+	// });
 </script>
+
+<h1>This is the temporary work page</h1>
