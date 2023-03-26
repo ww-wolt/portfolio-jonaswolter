9dfc9f38d9d9f97c9b14fc7e285e64696dc1dc70
Jonas Wolter <jonas.wolter.ch@gmail.com>
2023-03-26 12:35:34 +0200

    Add noise graphic to background

--- src/lib/components/Background.svelte
+++ src/lib/components/Background.svelte
@@ -1,1 +1,5 @@
 <div class="h-screen-large fixed top-0 left-0 -z-40 w-screen bg-[url('$lib/graphics/blurry-bg.jpg')] bg-cover" />
+<div
+	class="fixed top-0 left-0 -z-30 h-screen w-screen bg-[url('$lib/graphics/noise.png')] bg-[length:500px] mix-blend-overlay"
+	style="image-rendering: crisp-edges;"
+/>
