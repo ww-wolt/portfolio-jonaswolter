7d2022bb7e8984462e8b4a7f61fb4686c86c7189
Jonas Wolter <jonas.wolter.ch@gmail.com>
2023-04-03 03:54:54 +0200

    Navigation responsive font sizes

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -15,7 +15,7 @@
 		on:click={() => {
 			navOpen = !navOpen;
 		}}
-		class="group fixed top-4 left-6 z-50 flex items-center justify-start sm:top-4 sm:left-9 md:top-6 md:left-12 lg:top-7 lg:left-16 xl:top-8 xl:left-20"
+		class="group fixed left-6 top-4 z-50 flex items-center justify-start sm:left-9 sm:top-4 md:left-12 md:top-6 lg:left-16 lg:top-7 xl:left-20 xl:top-8"
 	>
 		<img class="w-6 md:w-7 xl:w-8" src={navOpen ? closeIcon : menuIcon} alt="" />
 		<span
@@ -26,23 +26,23 @@
 		>
 	</button>
 	<div
-		class="fixed z-40 h-0 w-screen overflow-hidden bg-neutral-900 text-7xl text-white transition-all duration-500"
+		class="fixed z-40 h-0 w-screen overflow-hidden bg-neutral-900 text-5xl text-white transition-all duration-500 md:text-6xl lg:text-7xl"
 		class:show={navOpen}
 		class:h-screen-large={navOpen}
 	>
 		<div class="h-screen-large flex w-screen flex-col items-center justify-center">
-			<a href="/#work" on:click={() => (navOpen = false)} class="menu-item relative my-4 rounded-[1000px] px-4 py-2 ">WORK</a>
-			<a href="/about" on:click={() => (navOpen = false)} class="menu-item relative my-4 rounded-[1000px] px-4 py-2  ">ABOUT</a>
-			<a href="/contact" on:click={() => (navOpen = false)} class="menu-item relative my-4 rounded-[1000px] px-4 py-2  ">CONTACT</a>
+			<a href="/#work" on:click={() => (navOpen = false)} class="menu-item relative my-2 px-4 py-2 lg:my-4">WORK</a>
+			<a href="/about" on:click={() => (navOpen = false)} class="menu-item relative my-2 px-4 py-2 lg:my-4">ABOUT</a>
+			<a href="/contact" on:click={() => (navOpen = false)} class="menu-item relative my-2 px-4 py-2 lg:my-4">CONTACT</a>
 			<div class="mt-8 flex items-center justify-center">
-				<a href="https://www.linkedin.com/in/jonas-wolter/" class="p-3">
-					<img src={linkedinIcon} alt="LinkedIn" />
+				<a href="https://www.linkedin.com/in/jonas-wolter/" class="m-4">
+					<img class="h-6 w-6 lg:h-8 lg:w-8" src={linkedinIcon} alt="LinkedIn" />
 				</a>
-				<a href="https://github.com/moonclimber1" class="p-3">
-					<img src={githubIcon} alt="Github" />
+				<a href="https://github.com/moonclimber1" class="m-4">
+					<img class="h-6 w-6 lg:h-8 lg:w-8" src={githubIcon} alt="Github" />
 				</a>
-				<a href="https://vimeo.com/jonaswolter" class="p-3">
-					<img src={vimeoIcon} alt="Vimeo" />
+				<a href="https://vimeo.com/jonaswolter" class="m-4">
+					<img class="h-6 w-6 lg:h-8 lg:w-8" src={vimeoIcon} alt="Vimeo" />
 				</a>
 				<!-- <a href="https://www.instagram.com/moonclimber_/" class="p-3">
 					<img src={instagramIcon} alt="Instagram" />
