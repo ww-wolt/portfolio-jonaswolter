7ea6f97475d68b84824aebbafb9e9cea8593bb69
Jonas Wolter <jonas.wolter.ch@gmail.com>
2025-05-04 18:25:36 +0200

    Add speed insights

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,4 +1,8 @@
+// Simple Vercel Statistics
 import { dev } from '$app/environment';
 import { injectAnalytics } from '@vercel/analytics/sveltekit';
-
 injectAnalytics({ mode: dev ? 'development' : 'production' });
+
+// Vercel Page Speed Insights
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+injectSpeedInsights();
